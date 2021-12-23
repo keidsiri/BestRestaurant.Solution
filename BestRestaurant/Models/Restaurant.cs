b9b1bddac7032140d2e01cc120f96acf4fecf20f
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-22 16:03:41 -0800

    Add folders and files in View
    Co-authored-by: Curtis Brooks <curtisbrooks678@gmail.com>
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>

---
+++ BestRestaurant/Models/Restaurant.cs
@@ -0,0 +1,13 @@
+namespace BestRestaurant.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Capacity { get; set; }
+    public string Location {get; set; }
+    public int CuisineId { get; set; }
+    public virtual Category Cuisine { get; set; }
+  }
+}
